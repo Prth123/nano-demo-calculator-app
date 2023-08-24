53e58f5924413cfcb8f4725923eb9ad16996c1ad
prth123 <dearparthsingh@google.com>
2023-08-24 13:12:49 +0530

    first

--- routes.cpp
+++ routes.cpp
@@ -1,16 +1,58 @@
+// #include "crow_all.h"
+
+// crow::response greet()
+// {
+//     return crow::response{""};
+// }
+// crow::response add(const crow::request &req)
+// {
+//     auto input = crow::json::load(req.body);
+//     return crow::response{""};
+// }
+// crow::response subtract(const crow::request &req)
+// {
+//     auto input = crow::json::load(req.body);
+//     return crow::response{""};
+// }
 #include "crow_all.h"
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
+
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first + second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
+
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first - second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
